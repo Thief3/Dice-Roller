c47d064679c7982fd714287d2ae42103eb0ea987
Thief3 <kissarlim@gmail.com>
2020-05-20 19:56:24 +0100

    Changed license on README.md and changed the data structure for for add-conditional-value to use hashmaps and allow us a better way to access multiple effects tied to one die roll.

--- src/dice_roller/core.clj
+++ src/dice_roller/core.clj
@@ -16,6 +16,11 @@
   (repeatedly x #(rand-int y)))
 
 (defn add-conditional-value
-  "Adds a new dice value x, with an effect y, to a list of other effects z"
+  "Adds a new dice value x, with an effect y, as a hashmap to a list of other hashmaps z"
   [x y z]
-  (conj z (list x y)))
+  (assoc z x (conj (get z x) y)))
+  
+(defn effects-activated
+  "Checks a list of hashmaps x for values in list y, displaying any other values in their list"
+  [x y]
+  (println "no"))
