bceb1094ff1fe7c65d3f46abada16b1b6f7e3d38
Thief3 <kissarlim@gmail.com>
2020-05-21 23:30:03 +0100

    Changed so we can remove individual effects.

--- src/dice_roller/core.clj
+++ src/dice_roller/core.clj
@@ -9,16 +9,17 @@
   (map inc (repeatedly x #(rand-int y))))
 
 (defn add-conditional-value
-  "Adds a new dice value x, with an effect y, as a hashmap to a list of other hashmaps z"
+  "Adds a new dice value x, with an effect y, as a hashmap to a list of other hashmaps z."
   [x y z]
-  (assoc z x (conj (get z x) y)))
-  
+  (assoc z x (assoc (get z x) (gensym) y)))
+
 (defn effects-activated
-  "Checks a list of hashmaps x for values in list y, displaying any other values in their list"
+  "Checks a hashmap of hashmaps x for values in list y, displaying any other values in their hashmap."
   [x y]
   (flatten
-   (vals
-    (select-keys x y))))
+   (map
+    #(vals %) (vals
+               (get-in x y)))))
 
 (defn user-input
   "The main loop for the app."
